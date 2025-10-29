5cfbdbe288fe17656f3c28517dbb16ad361c4af4
lgmaestropiedra <guillermo-maestropiedra@hotmail.com>
2025-10-29 15:02:45 -0300

    Update SimpleTemplateWriter

--- src/plataforma1/reporting/SimpleTemplateWriter.java
+++ src/plataforma1/reporting/SimpleTemplateWriter.java
@@ -1,13 +1,13 @@
 package plataforma1.reporting;
 
 import freemarker.template.Configuration;
-import freemarker.template.ObjectWrapper;
 import freemarker.template.SimpleHash;
 import freemarker.template.Template;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
 
 /**
@@ -15,31 +15,30 @@ import java.util.logging.Logger;
  */
 public class SimpleTemplateWriter {
 
-    private static final Logger logger = Logger.getLogger(ReporteTabularPdfWriter.class.getName());
+    private static final Logger logger = Logger.getLogger(SimpleTemplateWriter.class.getName());
 
     /**
      * Genera html utilizando como salida outputStream.
      *
-     * @param data                     POJO que se utiliza como base de navegación de grafo en la plantilla, referenciado en la misma como "data"
-     * @param classForTempleateLoading clase a partir de cuyo classloader se cargan los archivos de plantilla
-     * @param templateSpec             especificación del archivo de plantilla
-     * @param outputStream             salida del archivo HTML
+     * @param data                    POJO que se utiliza como base de navegación de grafo en la plantilla, referenciado en la misma como "data"
+     * @param classForTemplateLoading clase a partir de cuyo classloader se cargan los archivos de plantilla
+     * @param templateSpec            especificación del archivo de plantilla
+     * @param outputStream            salida del archivo HTML
      * @throws Exception
      */
-    public void writeDocument(Object data, Class classForTempleateLoading, String templateSpec, OutputStream outputStream) throws Exception {
-        Template template = createConfiguration(classForTempleateLoading).getTemplate(templateSpec);
+    public void writeDocument(Object data, Class<?> classForTemplateLoading, String templateSpec, OutputStream outputStream) throws Exception {
+        Template template = createConfiguration(classForTemplateLoading).getTemplate(templateSpec);
         SimpleHash rootModel = new SimpleHash();
         rootModel.put("data", data);
-        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
-        template.process(rootModel, writer, ObjectWrapper.BEANS_WRAPPER);
-        writer.flush();
+
+        try (OutputStreamWriter writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)) {
+            template.process(rootModel, writer);
+        }
     }
 
-    private Configuration createConfiguration(Class classForTempleateLoading) throws IOException {
-        Configuration configuration = new Configuration();
-        configuration.setClassForTemplateLoading(classForTempleateLoading, "");
-        //configuration.setDirectoryForTemplateLoading(new File("./"));
-        configuration.setObjectWrapper(ObjectWrapper.DEFAULT_WRAPPER);
+    private Configuration createConfiguration(Class<?> classForTemplateLoading) throws IOException {
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_32);
+        configuration.setClassForTemplateLoading(classForTemplateLoading, "");
         return configuration;
     }
 }
